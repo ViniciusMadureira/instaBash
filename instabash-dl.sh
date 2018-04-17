e06df236adb092031ea2deac229da734780a3176
Vinícius Madureira <ViniciusMadureira@users.noreply.github.com>
2018-04-17 07:18:40 -0300

    Update instabash-dl.sh

--- instabash-dl.sh
+++ instabash-dl.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 # Author: Vinícius Madureira <viniciusmadureira@outlook.com>
-# Version: 0.1
-# Date: March 11th, 2018
+# Version: 0.12
+# Date: April 17th, 2018
 # Script: Bash Instagram Downloader (instabash-dl)
 # Dependencies: GNU bash 4.3.42(1); GNU coreutils 8.24; curl 7.43.0; GNU Wget 1.18; GNU grep 2.22; GNU sed 4.2.2
 
+# Download medias
 function download() {
 	for page in $pages; do
 		current_page=$(curl --silent --location "https://www.instagram.com/p/$page/")
 		medias_url=$(echo "$current_page" | grep --perl-regexp --only-matching '(?U)https.*\.(jpg|mp4)' | head -n 2 | awk '!seen[$0]++')	
 		for media_url in $medias_url; do
-			printf -v filename "%s%06d%s" "$username"'_' $((count++)) ".${media_url##*.}"
+			printf -v filename "%s%06d%s" "$account_name"'_' $((count++)) ".${media_url##*.}"
 			wget --quiet "$media_url" --output-document="$filename"
 		done
 	done
 }
 
-username="$1"
+# Login
+curl --silent --cookie-jar "cookies.txt" "https://www.instagram.com" > /dev/null
+csrftoken=$(cat cookies.txt | grep csrftoken | awk '{print $7}')
+curl --silent --cookie 'cookies.txt' --cookie-jar 'cookies.txt' --dump-header 'dump.txt' --globoff 'https://www.instagram.com/accounts/login/ajax/' -H 'Referer: https://www.instagram.com/accounts/login/' -H "X-CSRFToken: $csrftoken" --data-urlencode "username=$1" --data-urlencode "password=$2" --data-urlencode "queryParams={}" > /dev/null
+
+# Session settings
+account_name="$1"
 count=1
-query_hash='472f257a40c653c64c666ce877d59d2b'
-first_page=$(curl --silent "https://www.instagram.com/$username/")
+query_hash='42323d64886122307be10013ad2dcc44'
+first_page=$(curl --silent --cookie 'cookies.txt' --cookie-jar 'cookies.txt' "https://www.instagram.com/$account_name/")
 json_data=$(echo "$first_page" | grep --perl-regexp --only-matching '{"activity_counts".*(?=;)')
 id=$(echo "$first_page" | grep --perl-regexp --only-matching '{"id":"\d+"}' | sed --expression='s/[^[:digit:]]//g' --expression='1!d')
+shbid=$(cat dump.txt  | grep --perl-regexp --only-matching 'shbid=.*(?=;)'| sed --expression 's/.*=//g')
+sessionid=$(cat dump.txt  | grep --perl-regexp  '(?U)sessionid=.*(?=;)' | sed --expression 's/.*sessionid=//g')
 end_cursor=$(echo "$first_page" | grep --perl-regexp --only-matching '(?U){"has_next_page":true.*"}' | tail -n 1 | sed --expression='s/"}\|.*:"//g')
-while :; do
+
+# Download pages
+while [[ -n $end_cursor ]]; do
 	pages=$(echo "$json_data" | grep --perl-regexp --only-matching '(?U)"shortcode":".*"' | sed --expression='s/.*:\|"//g')
-	download "$pages" "$username"
-	json_data=$(curl --silent --globoff "https://www.instagram.com/graphql/query/?query_hash=$query_hash&variables={\"id\":\"$id\",\"first\":12,\"after\":\"$end_cursor\"}")
-	if [ -z $end_cursor ]; then
-		break;
-	fi
+	download "$pages" "$account_name"
+	json_data=$(curl --silent --globoff --cookie "cookies.txt" --cookie-jar 'cookies.txt' "https://www.instagram.com/graphql/query/?query_hash=$query_hash&variables={\"id\":\"$id\",\"first\":12,\"after\":\"$end_cursor\"}" -H "Cookie: shbid=$shbid; sessionid=$sessionid}")
 	end_cursor=$(echo "$json_data" | grep --perl-regexp --only-matching '(?U){"has_next_page":true.*"}' | tail -n 1 | sed --expression='s/"}\|.*:"//g')
 done
 exit 0
 
-#./instabash-dl.sh "viniciusit"
+#./instabash-dl "username" "password" "account_to_download_medias"
